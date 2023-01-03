8e6c8ebc76f47ac07d729b884e708a522a434c24
Narasimhulu <44318990+narsimha-dev@users.noreply.github.com>
2023-01-03 11:18:25 +0530

    clean up

--- src/main/java/com/product/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.product.productservice.repository;
-
-import com.product.productservice.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,String>
-{
-
-}
